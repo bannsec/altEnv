890c00932cd960076b3742b348659847ac4b5739
Owlz <whootandahalf@gmail.com>
2016-06-02 13:40:20 -0400

    Mostly working Debian MIPS installer

--- installers/MIPS/Debian.py
+++ installers/MIPS/Debian.py
@@ -6,6 +6,9 @@ import subprocess
 from helpers import *
 import re
 import urllib
+import os.path
+import os
+from tcolors import *
 
 NAME = "Debian MIPS"
 DESCRIPTION = "Installer for Debian running on an emulated MIPS Processor"
@@ -19,40 +22,65 @@ def setup(_):
      config : dict
         Dictionary object containing parsed config values
     """
-    readConfig()
-    tools = getTools()
-    #print(type(tools))
+    
+    base_url = "http://ftp.de.debian.org/debian/dists/stable/main/installer-mips/current/images/malta/netboot/"
 
-    with urllib.request.urlopen("http://ftp.de.debian.org/debian/dists/stable/main/installer-mips/current/images/malta/netboot/") as u:
+    config = readConfig()
+    tools = getTools()
+
+    # TODO: This ins't terribly pythonic...
+    while True:
+        env_name = input("Name your environment: ")
+        # Full path to this new environment
+        full_env_path = os.path.join(config['global']['base_path'],"environments",env_name)
+        if os.path.exists(full_env_path):
+            print("This environment name already exists. Try again.")
+            continue
+        else:
+            # Create the new env
+            os.mkdir(full_env_path)
+            break
+    
+    # How big should the hard drive be 
+    hd_size = input("Hard Drive Size [2G]: ")
+    # Default size of 2G
+    hd_size = hd_size if hd_size is not "" else "2G"
+
+    with urllib.request.urlopen(base_url) as u:
         html = u.read()
 
 
     # Figure out the versions
     vmlinux = re.search(b"<a href=\"(vmlinux.*?)\">",html).group(1).decode('ascii')
     
-    print("\nDownloading {0}".format(vmlinux))
-
-
-    # Download the needed config files
-    # http://ftp.de.debian.org/debian/dists/stable/main/installer-mips/current/images/malta/netboot/
-
-    #sys.stdout.write("Downloading Need Config Files ... ")
-    #sys.stdout.flush()
-    #urllib.request.urlretrieve("http://wiki.qemu-project.org/download/{0}.tar.bz2".format(qemu_version),os.path.join(config['global']['base_path'],"{0}.tar.bz2".format(qemu_version)))
-    #sys.stdout.write(green("[ Completed ]\n"))
-    
-    sys.stdout.write("Decompressing Archives ... ")
+    sys.stdout.write("\nDownloading {0} ... ".format(vmlinux))
     sys.stdout.flush()
 
-    lzma.decompress
-
-    # Create hard drive space
-    #qemu-img create -f qcow hda.img 2G
+    # Download vmlinux file
+    urllib.request.urlretrieve(base_url + vmlinux,os.path.join(full_env_path,vmlinux))
     
-    # In [14]: f = lzma.open("/media/bill/7a704ddd-c815-4d04-9454-5c027f381cb3/QEMU/altEnvs/FreeBSD_i386/FreeBSD-10.3-RELEASE-i386-bootonly.iso.xz")
-    # subprocess.check_output("xz -d /media/bill/7a704ddd-c815-4d04-9454-5c027f381cb3/QEMU/altEnvs/FreeBSD_i386/FreeBSD-10.3-RELEASE-i386-bootonly.iso.xz",shell=True)
-    #subprocess.check_output("xz -d /media/bill/7a704ddd-c815-4d04-9454-5c027f381cb3/QEMU/altEnvs/FreeBSD_i386/FreeBSD-10.3-RELEASE-i386-bootonly.iso.xz",shell=True,cwd="/media/bill/7a704ddd-c815-4d04-9454-5c027f381cb3/QEMU/altEnvs/FreeBSD_i386/")
+    sys.stdout.write(green("[ Completed ]\n"))
 
+    sys.stdout.write("Downloading initrd.gz ... ")
+    sys.stdout.flush()
+    
+    # Download initrd.gz
+    urllib.request.urlretrieve(base_url + "initrd.gz",os.path.join(full_env_path,"initrd.gz"))
 
+    sys.stdout.write(green("[ Completed ]\n"))
+
+    sys.stdout.write("Creating virtual hard drive ... ")
+    # TODO: Probably should check output
+    subprocess.check_output("qemu-img create -f qcow {0} {1}".format(os.path.join(full_env_path,"hda.img"),hd_size),shell=True)
+
+    sys.stdout.write(green("[ Completed ]\n"))
+   
+    print("Starting setup ... ") 
+    
     # Run system to initiate setup
-    #qemu-system-mips -M malta -kernel vmlinux-<version>-qemu -initrd initrd.gz -hda hda.img -append "root=/dev/ram console=ttyS0" -nographic
+    os.system("{0} -M malta -kernel {1} -initrd {2} -hda {3} -append \"root=/dev/ram console=ttyS0\" -nographic".format(
+        tools['qemu-system-mips'],
+        os.path.join(full_env_path,vmlinux),
+        os.path.join(full_env_path,"initrd.gz"),
+        os.path.join(full_env_path,"hda.img")
+        ))
