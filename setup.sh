ec4e6d9c56723d66fe33755fb560f90f7c0a2294
Owlz <whootandahalf@gmail.com>
2016-06-04 01:39:52 -0400

    Initial attempt at setup script

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Check OS
+. setup_detectOS.sh
+
+if [[ "$OS" = "linux" ]] && [[ "$DistroBasedOn" = "debian" ]]; then
+    sudo apt-get update
+    sudo apt-get install -y make gcc virtualenv virtualenvwrapper python3
+else
+    echo "Don't know prereqs for your distro. You should help me!"
+fi
+
+# Figure out where we are
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ALTENV_VENV="${HOME}/.virtualenvs/altEnv"
+
+# Setup the venv
+mkdir -p $ALTENV_VENV 2>/dev/null
+
+# Setup new virtual environment
+virtualenv -p $(which python3) "$ALTENV_VENV"
+
+# Activate it
+source "${ALTENV_VENV}/bin/activate"
+
+# Install requirements
+pip install -r ${DIR}/requirements.txt
+
+pushd .
+cd ${DIR}/MenuSystem-1.3
+
+python setup.py install
+
+popd 2>/dev/null
+
+echo "Install complete."
+echo "Remember to activate the altEnv virtualenv before using '$ source ${ALTENV_VENV}/bin/activate)'"
