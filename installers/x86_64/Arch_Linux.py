26902e08092eb6a50f730391ede5e2a5709e67f8
Owlz <whootandahalf@gmail.com>
2016-06-06 21:54:25 -0400

    Updates

--- installers/x86_64/Arch_Linux.py
+++ installers/x86_64/Arch_Linux.py
@@ -21,24 +21,36 @@ def setup(_):
     """
     print()
 
-    url = "https://releng.archlinux.org/pxeboot/ipxe_text.lkrn"
-
     config = readConfig()
     tools = getTools()
 
-    env_name, full_env_path, hd_size, smp, memory, input_type = getVMVariables()
+    env_name, full_env_path, hd_size, smp, memory, input_type, optimize = getVMVariables()
+
+    # Find the current url
+    with urllib.request.urlopen("https://www.archlinux.org/releng/netboot/") as f:
+        html = f.read()
+
+    # Parse it out
+    url = re.search(b"href=['\"](.*?)['\"]>ipxe.lkrn",html).group(1).decode('ascii')
+
 
     sys.stdout.write("\nDownloading PXE Boot ... ")
     sys.stdout.flush()
 
     # Download vmlinux file
-    urllib.request.urlretrieve(url,os.path.join(full_env_path,"ipxe_text.lkrn"))
+    urllib.request.urlretrieve(url,os.path.join(full_env_path,"ipxe.lkrn"))
     
     sys.stdout.write(green("[ Completed ]\n"))
 
+
     sys.stdout.write("Creating virtual hard drive ... ")
-    # TODO: Probably should check output
-    subprocess.check_output("{2} create -f qcow {0} {1}".format(os.path.join(full_env_path,"hda.img"),hd_size,tools['qemu-img']),shell=True)
+    sys.stdout.flush()
+
+    if optimize:
+        subprocess.check_output("{2} create -f raw {0} {1}".format(os.path.join(full_env_path,"hda.img"),hd_size,tools['qemu-img']),shell=True)
+    else:
+        subprocess.check_output("{2} create -f qcow {0} {1}".format(os.path.join(full_env_path,"hda.img"),hd_size,tools['qemu-img']),shell=True)
+
 
     sys.stdout.write(green("[ Completed ]\n"))
   
@@ -47,9 +59,21 @@ def setup(_):
     options = {
         'm': memory,
         'smp': str(smp),
-        'M': "pc,accel=kvm:xen:tcg",
-        'hda': '$ENV_PATH/hda.img'
     }
+
+    if optimize:
+        options['drive'] = 'file=$ENV_PATH/hda.img,if=virtio,cache=writeback,format=raw'
+        drive = "-drive {0}".format(options['drive'].replace("$ENV_PATH",full_env_path))
+
+        options['M'] = "'pc,accel=kvm:xen:tcg'"
+        M = "-M {0}".format(options['M'])
+
+    else:
+        options['hda'] = '$ENV_PATH/hda.img'
+        drive = "-hda {0}".format(options['hda'].replace("$ENV_PATH",full_env_path))
+
+        M = ""
+
 
     input_option = writeVMConfig(env_path=full_env_path,tool="qemu-system-x86_64",input_type=input_type,options=options)
 
@@ -58,11 +82,13 @@ def setup(_):
     print("Starting setup ... ") 
     
     # Run system to initiate setup
-    os.system("{0} -hda {1} -M pc,accel=kvm:xen:tcg -nographic -kernel {2} -smp {3} -m {4} {5}".format(
+    os.system("{0} {1} {6} -nographic -kernel {2} -smp {3} -m {4} {5}".format(
         tools['qemu-system-x86_64'],
-        os.path.join(full_env_path,"hda.img"),
-        os.path.join(full_env_path,"ipxe_text.lkrn"),
+        #os.path.join(full_env_path,"hda.img"),
+        drive,
+        os.path.join(full_env_path,"ipxe.lkrn"),
         smp,
         memory,
-        input_option
+        input_option,
+        M
         ))
