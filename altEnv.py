d9c8c034aabc350aecdf8fa502a86318555ef69e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-25 07:43:07 +0000

    Adding tarfile member sanitization to extractall()

--- altEnv.py
+++ altEnv.py
@@ -62,7 +62,26 @@ def installQEMU(_):
     sys.stdout.flush()
     
     with tarfile.open("{0}.tar.bz2".format(qemu_version)) as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
 
     sys.stdout.write(green("[ Completed ]\n"))
     
